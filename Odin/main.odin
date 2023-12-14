661f4d249ca9269a2aec109f09121dcdaedc590b
Matthew Crews <matthew@crews.email>
2023-12-14 10:14:00 -0800

    update

--- Odin/main.odin
+++ Odin/main.odin
@@ -1,8 +1,25 @@
 package main
 
+import "core:runtime"
+
 @export
-add_arrays :: proc "c" (result, a, b: [^]f64, length: i32) {
+bench1 :: proc "c" (result, a, b: [^]i32, length: i32) {
 	for i := i32(0); i < length; i += 1 {
-		result[i] = a[i] + b[i]
+		if a[i] < b[i] {
+			result[i] = a[i] + b[i]
+		} else {
+			result[i] = a[i] - b[i]
+		}
+	}
+}
+
+@export
+bench2 :: proc "c" (result, a, b: [^]i32, length: i32) {
+	for i := i32(0); i < length; i += 1 {
+		if a[i] < b[i] {
+			result[i] = a[i] + b[i] / a[i]
+		} else {
+			result[i] = a[i] / b[i] - b[i]
+		}
 	}
 }
--- Program.fs
+++ Program.fs
@@ -7,47 +7,81 @@ open FSharp.Core
 open BenchmarkDotNet.Attributes
 open FSharp.NativeInterop
 
+#nowarn "9"
+
 module Odin =
 
-    let [<Literal>] DLLName = "Odin/Odin"
-    type FloatPtr = nativeptr<float>
+    let [<Literal>] DLLName = "Odin"
+    type IntPtr = nativeptr<int>
+
     [<DllImport(DLLName, CallingConvention=CallingConvention.Cdecl)>]
-    extern void add_arrays(FloatPtr result, FloatPtr a, FloatPtr b, int32 length)
+    extern void bench1(IntPtr result, IntPtr a, IntPtr b, int32 length)
+
+    [<DllImport(DLLName, CallingConvention=CallingConvention.Cdecl)>]
+    extern void bench2(IntPtr result, IntPtr a, IntPtr b, int32 length)
 
 
 type Benchmarks () =
 
-    let size = 1_000
-    let a = Array.create size 1.0
-    let b = Array.create size 2.0
-    let fResult = Array.zeroCreate size
+    let rng = Random 123
+    let size = 100_000
+    let a = Array.init size (fun _ ->
+        rng.Next())
+    let b = Array.init size (fun _ ->
+        rng.Next())
+    let result = Array.zeroCreate size
     let oResult = Array.zeroCreate size
 
     [<Benchmark>]
-    member _.FSharp () =
+    member _.FSharp_Bench1 () =
 
         (a, b)
         ||> Array.iteri2 (fun i aValue bValue ->
-            fResult[i] <- aValue + bValue)
+            if aValue < bValue then
+                result[i] <- aValue + bValue
+            else
+                result[i] <- aValue - bValue)
 
-        fResult
+        result
 
     [<Benchmark>]
-    member _.Odin () =
+    member _.Odin_Bench1 () =
 
         use aPtr = fixed a
         use bPtr = fixed b
         use resultPtr = fixed oResult
 
-        Odin.add_arrays(resultPtr, aPtr, bPtr, a.Length)
+        Odin.bench1(resultPtr, aPtr, bPtr, a.Length)
+        oResult
+
+    [<Benchmark>]
+    member _.FSharp_Bench2 () =
+
+        (a, b)
+        ||> Array.iteri2 (fun i aValue bValue ->
+            if aValue < bValue then
+                result[i] <- aValue + bValue / aValue
+            else
+                result[i] <- aValue / bValue - bValue)
+
+        result
+
+    [<Benchmark>]
+    member _.Odin_Bench2 () =
+
+        use aPtr = fixed a
+        use bPtr = fixed b
+        use resultPtr = fixed oResult
+
+        Odin.bench2(resultPtr, aPtr, bPtr, a.Length)
         oResult
 
 
 let test () =
 
     let b = Benchmarks()
-    let fsharpResult = b.FSharp()
-    let odinResult = b.Odin()
+    let fsharpResult = b.FSharp_Bench1()
+    let odinResult = b.Odin_Bench1()
     printfn $"{fsharpResult = odinResult}"
 
 // test()
