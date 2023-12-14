f12db0ecb120e9e22ea85ac5ab65c8d12d17c8eb
Matthew Crews <matthew@crews.email>
2023-12-14 14:15:04 -0800

    update

--- Odin/main.odin
+++ Odin/main.odin
@@ -1,6 +1,7 @@
 package main
 
 import "core:runtime"
+import "core:slice"
 
 @export
 bench1 :: proc "c" (result, a, b: [^]i32, length: i32) {
@@ -17,9 +18,34 @@ bench1 :: proc "c" (result, a, b: [^]i32, length: i32) {
 bench2 :: proc "c" (result, a, b: [^]i32, length: i32) {
 	for i := i32(0); i < length; i += 1 {
 		if a[i] < b[i] {
-			result[i] = a[i] + b[i] / a[i]
+			result[i] = a[i] + b[i] * a[i]
 		} else {
-			result[i] = a[i] / b[i] - b[i]
+			result[i] = a[i] * b[i] - b[i]
 		}
 	}
+}
+
+@export
+bench3 :: proc "c" (#no_alias result, a, b: [^]i32, length: i32) {
+	for i := i32(0); i < length; i += 1 {
+		if a[i] < b[i] {
+			result[i] = b[i] * a[i]
+		} else {
+			result[i] = a[i] * b[i]
+		}
+	}
+
+	for i := i32(0); i < length; i += 1 {
+		if a[i] < b[i] {
+			result[i] = result[i] + a[i]
+		} else {
+			result[i] = result[i] - b[i]
+		}
+	}
+}
+
+@export
+min :: proc "c" (a: []i32) -> i32 {
+	context = runtime.default_context()
+	return slice.min(a)
 }
--- Program.fs
+++ Program.fs
@@ -11,6 +11,33 @@ open FSharp.NativeInterop
 
 module Odin =
 
+    open System.Runtime.InteropServices
+    open Microsoft.FSharp.NativeInterop
+
+    type Slice<'a when 'a : unmanaged> =
+        struct
+           val Pointer: nativeptr<'a>
+           val Length: nativeint
+
+           new (ptr, l) =
+               {
+                   Pointer = ptr
+                   Length = nativeint l
+               }
+
+           member s.Item
+                with get (i: int) =
+                    NativePtr.get s.Pointer i
+                and set i v =
+                    NativePtr.set s.Pointer i v
+        end
+
+    module Slice =
+
+        let create<'a when 'a: unmanaged>(arr: 'a[]) =
+            use ptr = fixed arr
+            Slice (ptr, arr.Length)
+
     let [<Literal>] DLLName = "Odin"
     type IntPtr = nativeptr<int>
 
@@ -19,6 +46,13 @@ module Odin =
 
     [<DllImport(DLLName, CallingConvention=CallingConvention.Cdecl)>]
     extern void bench2(IntPtr result, IntPtr a, IntPtr b, int32 length)
+
+    [<DllImport(DLLName, CallingConvention=CallingConvention.Cdecl)>]
+    extern void bench3(IntPtr result, IntPtr a, IntPtr b, int32 length)
+
+    type IntSlice = Slice<int>
+    [<DllImport(DLLName, CallingConvention=CallingConvention.Cdecl)>]
+    extern void min(IntSlice a)
 
 
 type Benchmarks () =
@@ -60,9 +94,9 @@ type Benchmarks () =
         (a, b)
         ||> Array.iteri2 (fun i aValue bValue ->
             if aValue < bValue then
-                result[i] <- aValue + bValue / aValue
+                result[i] <- aValue + bValue * aValue
             else
-                result[i] <- aValue / bValue - bValue)
+                result[i] <- aValue * bValue - bValue)
 
         result
 
@@ -76,12 +110,34 @@ type Benchmarks () =
         Odin.bench2(resultPtr, aPtr, bPtr, a.Length)
         oResult
 
+    [<Benchmark>]
+    member _.FSharp_Min () =
+        Array.min a
+
+    [<Benchmark>]
+    member _.Odin_Min () =
+
+        let aSlice = Odin.Slice.create a
+
+        Odin.min(aSlice)
+
+    // [<Benchmark>]
+    // member _.Odin_Bench3 () =
+    //
+    //     use aPtr = fixed a
+    //     use bPtr = fixed b
+    //     use resultPtr = fixed oResult
+    //
+    //     Odin.bench3(resultPtr, aPtr, bPtr, a.Length)
+    //     oResult
+
+
 
 let test () =
 
     let b = Benchmarks()
-    let fsharpResult = b.FSharp_Bench1()
-    let odinResult = b.Odin_Bench1()
+    let fsharpResult = b.FSharp_Bench2()
+    let odinResult = b.Odin_Bench2()
     printfn $"{fsharpResult = odinResult}"
 
 // test()
